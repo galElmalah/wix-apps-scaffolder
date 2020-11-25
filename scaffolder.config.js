03700caaa91e2e3f0a85debfbad1c80d96576dcb
Gal Elmalah <gale@wix.com>
2020-11-25 14:37:13 +0200

    missing function error

--- scaffolder.config.js
+++ scaffolder.config.js
@@ -1,16 +1,28 @@
 const { exec } = require('child_process');
-const { promisify } = require('util')
+const { promisify } = require('util');
 
-const promisifyExec = promisify(exec)
+const promisifyExec = promisify(exec);
 
 module.export = {
+  functions: {
+    dotenvIgnore: () => '.dotenv'
+  },
   templatesOptions: {
-    "oauth-app": {
-      postTemplateGeneration: async () => {
-        const appPath = `${ctx.targetRoot}/${ctx.parametersValues.moduleName}`
+    'oauth-app': {
+      postTemplateGeneration: async (ctx) => {
+        const root = `${ctx.targetRoot}/wix-oauth-app-example`;
+        const appServerPath = `${root}/server`;
+        const appClientPath = `${root}/client`;
+        ctx.logger.info('Installing all deps in root, server and client')
+        await Promise.all([
+          promisifyExec('npm i', { cwd: root }),
+          promisifyExec('npm i', { cwd: appClientPath }),
+          promisifyExec('npm i', { cwd: appServerPath })]
+        );
 
-        await Promise.all(promisifyExec(''))
-      }
-    }
-  }
-}
+        ctx.logger.info('Starting server and client apps')
+
+      },
+    },
+  },
+};
--- scaffolder/oauth-app/wix-oauth-app-example/server /src/wixApi.js
+++ scaffolder/oauth-app/wix-oauth-app-example/server /src/wixApi.js
@@ -4,7 +4,7 @@ const requestManager = require('./requestManager');
 
 const { getAccessTokenFromWix } = requestManager;
 
-module.exports.getSiteInfo = async function     getSiteInfo(refreshToken) {
+module.exports.getSiteInfo = async function getSiteInfo(refreshToken) {
     const { access_token } = await getAccessTokenFromWix(refreshToken);
     const { data } = await axios.get('https://www.wixapis.com/apps/v1/instance', {
         headers: {
