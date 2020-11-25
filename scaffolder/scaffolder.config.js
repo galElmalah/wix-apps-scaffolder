7e62f35a6e8963d62d7511d80960ff722cae1d05
Gal Elmalah <gale@wix.com>
2020-11-25 15:21:41 +0200

    template is ready

---
+++ scaffolder/scaffolder.config.js
@@ -0,0 +1,33 @@
+const { exec, spawnSync } = require('child_process');
+const { promisify } = require('util');
+
+const promisifyExec = promisify(exec);
+
+module.exports = {
+  functions: {
+    dotenvIgnore: () => '.dotenv',
+  },
+  templatesOptions: {
+    'oauth-app': {
+      hooks: {
+        preAskingQuestions: (ctx) => console.log(ctx),
+        postTemplateGeneration: async (ctx) => {
+          const root = `${ctx.targetRoot}/wix-oauth-app-example`;
+          const appServerPath = `${root}/server`;
+          const appClientPath = `${root}/client`;
+          ctx.logger.info(`Installing all deps in root, server and client.\nThis may take a few seconds...`);
+          try {
+            await Promise.all([
+              promisifyExec('npm i', { cwd: root }),
+              promisifyExec('npm i', { cwd: appClientPath }),
+              promisifyExec('npm i', { cwd: appServerPath }),
+            ]);
+          } catch (e) {
+            ctx.logger.error(e)
+          }
+          ctx.logger.warning(`Finished Installing all dependencies.\nTo start your app go to "${root}" and run "npm start".\n`);
+        },
+      },
+    },
+  },
+};
